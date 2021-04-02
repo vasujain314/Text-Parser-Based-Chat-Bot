1b437d11adb7e0fa9eaeaf3b600c649a880cc666
vasujain314 <jain.vasu509@gmail.com>
2021-04-03 03:04:40 +0530

    changes

--- vishal.js
+++ vishal.js
@@ -1,35 +1,53 @@
 var str = [];
-str[0] = "is the weather cold?"
-str[1] = "    if yes, wear a warm jacket"
-str[2] = "    if no, is it raining?"
-str[3] = "        if yes, is it windy?"
-str[4] = "            if yes, wear a windbreaker"
-str[5] = "            if no, take an umbrella"
-str[6] = "        if no, then take a teeshirt" 
+// str[0] = "is the weather cold?"
+// str[1] = "    if yes, wear a warm jacket"
+// str[2] = "    if no, is it raining?"
+// str[3] = "        if yes, is it windy?"
+// str[4] = "            if yes, wear a windbreaker"
+// str[5] = "            if no, take an umbrella"
+// str[6] = "        if no, then take a teeshirt" 
 
-var n=6;
+var n;
+var obj;
+function input()
+{
+   str = $('#input').val().split("\n");
+   n = str.length -1; 
+    obj = new Array(n/2);
+   for (var i = 0; i < obj.length; i++) {
+      obj[i] = new Array(2);
+    }
 
-
-var obj = new Array(n/2); 
-for (var i = 0; i < obj.length; i++) {
-    obj[i] = new Array(2);
-}
 var rootques = String(str[0]);
-console.log(rootques);
+// console.log(rootques);
 document.getElementById("ques").innerHTML = rootques;
 
-
-for(var i=1;i<str.length;i++)
+// console.log(str[1]);
+for(var i=1;i<=n;i++)
 {
+    
+    console.log(str[i]);
     let size = str[i].search(/\S/);
-
-    let idx = size/4-1;
+   
+    if(size==1)
+    {
+        size=2;
+    }
+    console.log(size);
     let len = str[i].length;
-    obj[idx].push(str[i].substring(size, len));
-         
+    
+    obj[size/2-1].push(str[i].substring(size, len));      
 }
 
+// for(var i=0;i<n/2;i++)
+// {
+// console.log(obj[i][2]);
+// }
+}
+
+
 var level = 0;
+
 function vishalyes()
     {
         console.log(obj[level][2]);
@@ -69,7 +87,3 @@ function vishalNo()
 
 
 
-// for(var i=0;i<n/2;i++)
-// {
-//     console.log(obj[i][2]);
-// }
